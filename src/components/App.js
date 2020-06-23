ba85b895cd3dc41a3f00156214a446a32d4b3a11
Andrey Dorodnev <andrew992@mail.ru>
2020-06-23 15:14:45 +0300

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import '../components/App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>START</h1>
+        </div>
+    );
+}
+
+export default App;
