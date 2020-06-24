b7138a56ecc2890d8a2f71544999bfe9633bd5db
Andrey Dorodnev <andrew992@mail.ru>
2020-06-24 10:53:26 +0300

    header title and signOut btn add

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getUser} from '../../store/authReducer';
+import {signOut} from '../../store/loginUser';
 
-export default props => {
+function Header(props) {
     return (
         <div className="header">
-            HEADER
+            <h2>React test application</h2>
+            {
+                props.user?
+                <button onClick={props.signOut}>Sign Out</button>:
+                null
+            }
         </div>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        user: getUser(state.authState),
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        signOut: () => dispatch(signOut()),
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Header);
+
