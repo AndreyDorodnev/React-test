1c8655f22aa71552bb288ac677e3acbf65bfa4c0
Andrey Dorodnev <andrew992@mail.ru>
2020-06-24 18:50:43 +0300

    add address to desc

--- src/components/home/Description.js
+++ src/components/home/Description.js
@@ -14,6 +14,7 @@ function Description(props) {
                     <Card.Header as="h5">{props.data.name}</Card.Header>
                     <Card.Body>
                         <Card.Title>{`ЗП: ${getSalaryStr(props.data.salary)}`}</Card.Title>
+                        <Card.Text>{`Адрес: ${getAddressStr(props.data.address)}`}</Card.Text>
                         <Card.Text> {`Требования: ${getRequirement(props.data.snippet)}`}</Card.Text>
                         <Card.Text>{`Задачи: ${getResponsibilities(props.data.snippet)}`}</Card.Text>
                         <Button variant="primary" onClick={e=>props.onBtnClick(props.data.id)}>Подробнее</Button>
