1c632af74d8cceea2c5cfffa4969db288e8dec7a
Andrey Dorodnev <andrew992@mail.ru>
2020-06-24 18:52:05 +0300

    add requirement and responsibility getters

--- src/utils/vacancyUtils.js
+++ src/utils/vacancyUtils.js
@@ -12,3 +12,11 @@ export const getSalaryStr = (salary) => {
 export const getAddressStr = (address) => {
     return address? `${address.city}` : 'не указан';
 }
+
+export const getRequirement = (snippet) => {
+    return snippet.requirement? snippet.requirement : 'не указаны';
+}
+
+export const getResponsibilities = (snippet) => {
+    return snippet.responsibility? snippet.responsibility : 'не указаны';
+}
